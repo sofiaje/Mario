bd5963bf815af3d7f93cbb40165c0018ef5865e9
Sofia <sofia.jespersen@hotmail.com>
2023-03-23 11:19:41 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,107 @@
+let wrapper = document.getElementById("wrapper");
+let startGameBtn = document.querySelector(".startGame")
+let section = document.createElement("section")
+let darkMode = document.querySelector(".darkModeBtn")
+// let myAudio = document.querySelector('#audio')
+// myAudio.play()
+let mySound = new Audio('/assets/jump2.mp3')
+let hit = new Audio('/assets/hit2.mp3')
+console.log(hit)
+
+let index = 0
+document.body.addEventListener("keyup", (e) => {
+    if (e.code === 'Enter') {
+        renderGame()
+    }
+}, { once: true })
+
+function renderGame() {
+    let h1 = document.createElement("h1")
+    h1.innerText = "Watch out for the bump!";
+
+    let gameFrame = document.createElement("div")
+    gameFrame.classList.add("gameFrame")
+
+    let character = document.createElement("div")
+    character.classList.add("character")
+
+    let bump = document.createElement("div")
+    bump.classList.add("bump")
+
+    let img = document.createElement("img")
+    img.setAttribute("src", "assets/test.png")
+    img.classList.add("img")
+
+    let score = document.createElement("p")
+    score.classList.add("score")
+
+    wrapper.innerText = ""
+
+    character.append(img)
+    gameFrame.append(character, bump)
+    wrapper.append(h1, gameFrame, score)
+
+    gameFunction(score, character, bump)
+}
+
+function gameFunction(score, character, bump) {
+    let playerIsAlive = true
+    let counter = 0
+    score.innerHTML = `Score: ${counter}`
+
+
+    window.addEventListener("keyup", (e) => {
+        if (e.code === 'Space' && playerIsAlive) {
+            mySound.play()
+            character.classList.add("jump")
+            setTimeout(() => {
+                character.classList.remove("jump")
+            }, 500);
+            counter++
+            score.innerText = `Score: ${counter}`
+        }
+
+    })
+
+    let intervalId = setInterval(function () {
+        let characterBottom = parseInt(window.getComputedStyle(character).getPropertyValue("bottom"));
+        let bumpLeft = parseInt(window.getComputedStyle(bump).getPropertyValue("left"));
+        if (bumpLeft < 190 && bumpLeft > 90 && characterBottom <= 50) {
+            playerIsAlive = false
+            hit.play()
+
+            bump.style.animation = "none"
+            bump.style.left = bumpLeft + "px"
+            character.style.bottom = characterBottom + "px"
+            clearInterval(intervalId);
+            renderInfo()
+        }
+    }, 10);
+}
+
+function renderInfo() {
+    let h2 = document.createElement("h2");
+    h2.innerText = "Game over!"
+
+    let reloadBtn = createBtn("try again")
+    let infoBtn = createBtn("info")
+    wrapper.append(h2, reloadBtn, infoBtn)
+
+    reloadBtn.addEventListener("click", () => {
+        renderGame()
+    })
+
+    infoBtn.addEventListener("click", () => {
+        section.innerHTML = `<p>Hello! My name is Sofia, I'm a frontend developer student at Nackademin in Stockholm. Checkout my cv and portfolio at <a href="https://sofiaje.github.io/">sofiaje.github.io</a> or try again with the game, I think you'll get a hang of it pretty soon ;)</p>`
+        wrapper.append(section)
+    })
+}
+
+//helping function
+function createBtn(text) {
+    let btn = document.createElement("button")
+    btn.classList.add("btn")
+    btn.innerText = `${text}`
+    return btn;
+}
+
